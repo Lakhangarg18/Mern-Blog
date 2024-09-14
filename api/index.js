d26d9cc3ac5d8dbecede79c872c9ad3cc5ac88d5
Lakhan Garg <113020547+Lakhangarg18@users.noreply.github.com>
2024-09-14 19:03:57 +0530

    connecred with mongodb database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
-const app=express();
-app.listen(3000,()=>{
-  console.log("success");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+const app = express();
+app.listen(3000, () => {
+  console.log("success");
+});
