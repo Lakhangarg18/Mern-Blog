1be6a993abfd8ef8bc12a81f33638942974b5dd3
Lakhan Garg <113020547+Lakhangarg18@users.noreply.github.com>
2024-09-15 10:49:02 +0530

    added test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,7 @@ mongoose
 
 
 const app = express();
+app.use("/api/user",userRoutes);
 app.listen(3000, () => {
   console.log("success");
 });
