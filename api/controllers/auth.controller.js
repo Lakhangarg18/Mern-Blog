5a959d23dbe7595e48bb270efa351980f3eb8eee
Lakhan Garg <113020547+Lakhangarg18@users.noreply.github.com>
2024-09-19 16:38:28 +0530

    added redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -32,16 +32,16 @@ export const signup = async (req, res,next) => {
 export const signin=async(req,res,next)=>{
   const {email,password}=req.body;
   if(!email || !password || email==="" || password==""){
-    next(errorHandler((400,"All Fields are required")));
+    next(errorHandler(400,"All Fields are required"));
   }
   try{
     const validUser=await User.findOne({email});
     if(!validUser){
-     return next(errorHandler((400,"User not found")));
+     return next(errorHandler(404,"User not found"));
     }
     const validPassword=bcryptjs.compareSync(password,validUser.password);
     if(!validPassword){
-      return next((errorHandler((400,"Invalid Password"))));
+      return next(errorHandler(400,"Invalid Password"));
     }
     const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
     const { password:pass, ...rest }=validUser._doc;
